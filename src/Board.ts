2bf8d707d18021897e42f5678ea1c830960f249e
Ymaril <martinsep@yandex.ru>
2020-07-08 13:02:00 +0300

    Add image

--- src/Board.ts
+++ src/Board.ts
@@ -4,59 +4,81 @@ export class Board {
   private tiles: SVGRectElement[] = [];
   public onClick: (coordinates: Coordinates) => void = () => {};
 
-  constructor(
-    private svg: SVGGraphicsElement,
-    private size: number,
-    state: number[][]
-  ) {
-    state.forEach((row, y) => {
-      row.forEach((value, x) => this.createTile(value, { x, y }));
-    });
-
-    svg.onclick = (e) => {
-      const tile_size = this.getTileSize();
-      this.onClick({
-        x: Math.trunc(e.offsetX / tile_size.width),
-        y: Math.trunc(e.offsetY / tile_size.height),
-      });
-    };
+  constructor(private svg: SVGGraphicsElement, private size: number, state: number[][]) {
+    state.forEach((row, y) => row.forEach((value, x) => value && this.createTile(value, { x, y })));
+    svg.onclick = e => this.onBoardClick(e);
   }
 
-  public draw(state: number[][]) {
+  public redraw(state: number[][]) {
     state.forEach((row, y) => {
-      row.forEach((value, x) => this.moveTile(value, { x, y }));
+      row.forEach((value, x) => value && this.moveTile(this.tiles[value], { x, y }));
     });
   }
 
   private createTile(id: number, coordinates: Coordinates): SVGRectElement {
-    const rect = (this.tiles[id] = document.createElementNS(
+    const rect = document.createElementNS(
       "http://www.w3.org/2000/svg",
       "rect"
-    ));
+    );
+    Object
+      .entries(this.getDefaultTileAttributes())
+      .forEach(([attr, value]) => rect.setAttribute(attr, value.toString()));
+    this.moveTile(rect, coordinates);
+    this.svg.appendChild(rect);
+    this.tiles[id] = rect;
+    return this.tiles[id];
+  }
+
+  private getDefaultTileAttributes() {
     const tile_size = this.getTileSize();
-    Object.entries({
-      x: coordinates.x * tile_size.width,
-      y: coordinates.y * tile_size.height,
+
+    return {
       width: tile_size.width,
       height: tile_size.height,
       class: "tile",
-      fill: "none",
       stroke: "#000",
       "stroke-width": "2px",
-    }).forEach(([attr, value]) => rect.setAttribute(attr, value.toString()));
-    this.svg.appendChild(rect);
-    return rect;
+    };
   }
 
-  private moveTile(id: number, coordinates: Coordinates) {
+  private moveTile(tile: SVGRectElement, coordinates: Coordinates) {
     const tile_size = this.getTileSize();
 
     Object.entries({
       x: coordinates.x * tile_size.width,
       y: coordinates.y * tile_size.height,
     }).forEach(([axis, value]) =>
-      this.tiles[id].setAttribute(axis, value.toString())
+      tile.setAttribute(axis, value.toString())
     );
+  }
+
+  private onBoardClick(e: MouseEvent) {
+    const tile_size = this.getTileSize();
+    this.onClick({
+      x: Math.trunc(e.offsetX / tile_size.width),
+      y: Math.trunc(e.offsetY / tile_size.height),
+    });
+  }
+
+  public assignImage(id: string) {
+    this.tiles.forEach((tile, tile_id) => {
+      const pattern = document.createElementNS("http://www.w3.org/2000/svg", "pattern");
+      const pattern_id = `pat${tile_id}`;
+      Object.entries({
+        width: 1,
+        height: 1,
+        id: pattern_id
+      }).forEach(([attr, value]) => pattern.setAttribute(attr, value.toString()));
+      const image = document.createElementNS("http://www.w3.org/2000/svg", "use");
+      Object.entries({
+        href: `#${id}`,
+        x: -1 * tile.x.baseVal.value,
+        y: -1 * tile.y.baseVal.value
+      }).forEach(([attr, value]) => image.setAttribute(attr, value.toString()));
+      pattern.appendChild(image);
+      this.svg.querySelector('defs')?.appendChild(pattern);
+      tile.setAttribute("fill", `url(#${pattern_id})`);
+    });
   }
 
   private getTileSize() {
